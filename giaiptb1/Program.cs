c1a0307e5dbe487d575e9ace5020de8d97bc7c06
namduong2006 <129411334+namduong2006@users.noreply.github.com>
2023-04-02 20:46:36 +0700

    Add project files.

---
+++ giaiptb1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace giaiptb1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            float a, x, b;
+            Console.WriteLine("Giải phương trình bậc 1 có dạng: ax + b = 0");
+            Console.Write("Mời nhập a :");
+            a = float.Parse(Console.ReadLine());
+            Console.Write("Mời nhập b :");
+            b = float.Parse(Console.ReadLine());
+            x = (-b / a);
+            if (a != 0)
+                Console.WriteLine("PT có nghiệm x = " + x);
+            else if (a == 0 && b == 0)
+                Console.WriteLine("PT vố số nghiệm");
+            else
+                Console.WriteLine("PT vô nghiệm");
+            Console.ReadLine();
+        }
+    }
+}
